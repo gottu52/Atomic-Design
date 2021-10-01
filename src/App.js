b2203af2cef3931820fba04dfe5a7d3766819671
gottu52 <89571642+gottu52@users.noreply.github.com>
2021-10-01 22:37:56 +0900

    add-something

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
 import { PrimaryButton } from "./components/attoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/attoms/button/SecondaryButton";
 import { SerchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organism/user/UserCard";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
 
 const user = {
@@ -17,12 +19,14 @@ const user = {
 
 export default function App() {
   return (
-    <div className="App">
-      <PrimaryButton>テストってこと、、、？</PrimaryButton>
-      <SecondaryButton>うわあ、、、！！</SecondaryButton>
-      <br />
-      <SerchInput />
-      <UserCard user={user} />
-    </div>
+    <BrowserRouter>
+      <HeaderOnly className="App">
+        <PrimaryButton>テストってこと、、、？</PrimaryButton>
+        <SecondaryButton>うわあ、、、！！</SecondaryButton>
+        <br />
+        <SerchInput />
+        <UserCard user={user} />
+      </HeaderOnly>
+    </BrowserRouter>
   );
 }
