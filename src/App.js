2f929ce2a6368a2f87200898a130a732a989c03a
gottu52 <89571642+gottu52@users.noreply.github.com>
2021-10-02 00:10:22 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/attoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/attoms/button/SecondaryButton";
-import { SerchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organism/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { DefaultLayout } from "./components/templates/DeaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "ゆるふわ",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "papapapapapapapa@mail",
-  phone: "@@@-@@@-@@@@",
-  company: {
-    name: "株式会社rvrnxjq"
-  },
-  website: "http://g00gle.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テストってこと、、、？</PrimaryButton>
-        <SecondaryButton>うわあ、、、！！</SecondaryButton>
-        <br />
-        <SerchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,46 @@
 import styled from "styled-components";
+import { SerchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organism/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `ゆるふわ${val}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "papapapapapapapa@mail",
+    phone: "@@@-@@@-@@@@",
+    company: {
+      name: "株式会社rvrnxjq"
+    },
+    website: "http://g00gle.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
-      <h2>TOP</h2>
+      <h2>ユーザー一覧</h2>
+      <SerchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
